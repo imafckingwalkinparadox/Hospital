1229e1152ba3aa5fdabdb0cca5b532ceadbf84c4
imafckingwalkinparadox <hdvillatoro@scl.edu.gt>
2024-07-31 08:38:26 -0600

    diseñoLogin

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Main extends JFrame {
     public static void main(String[] args) {
         login login = new login();
+
+
+
 
     }
 }
--- src/login.java
+++ src/login.java
@@ -1,16 +1,32 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class login extends JFrame {
 
     public login (){
         this.setLayout(null);
-        this.setBounds(0,0,800,500);
-        this.setVisible(true);
+        this.setBounds(0,0,1800,1000);
+
+        JLabel email = new JLabel("Email");
+        email.setBounds(800,225,100,50);
+        this.add(email);
 
         JTextField usuario = new JTextField();
-        usuario.setBounds(300,20,200,50);
+        usuario.setBounds(800,260,350,40);
 
+        JLabel password = new JLabel("Contraseña");
+        password.setBounds(800,295,100,50);
+        this.add(password);
+
+        JTextField contraseña = new JTextField();
+        contraseña.setBounds(800,330,350,40);
+
+        JButton boton = new JButton("Login");
+        boton.setBounds(800,390,120,40);
+
+        this.add(boton);
         this.add(usuario);
+        this.add(contraseña);
         this.setVisible(true);
     }
 }
